2dc436ae1ce1db3dd7d8579335b42694b49d2823
Michael Toutonghi <toutonghi.michael@gmail.com>
2020-01-11 01:05:44 +0100

    Fix icon on Linux

--- routes/api/native/idRegistration.js
+++ routes/api/native/idRegistration.js
@@ -1,34 +1,6 @@
 const Promise = require('bluebird');
 
 module.exports = (api) => {    
-  api.native.register_id_name = (coin, token, name, controlAddress, referralId) => {
-    return new Promise((resolve, reject) => {      
-      let params = referralId ? [name, controlAddress, referralId] : [name, controlAddress]
-      api.native.callDaemon(coin, 'registernamecommitment', params, token)
-      .then((nameCommitmentResult) => {
-        if (
-          nameCommitmentResult &&
-          nameCommitmentResult.txid &&
-          nameCommitmentResult.namereservation
-        ) {
-          const localCommitments = api.loadLocalCommitments()
-          let saveCommitment = { ...nameCommitmentResult, controlAddress}
-
-          api.saveLocalCommitments({
-            ...localCommitments,
-            [coin]: localCommitments[coin] ? [...localCommitments[coin], saveCommitment] : [saveCommitment]
-          });
-          resolve({...saveCommitment, coin});
-        } else {
-          throw new Error(nameCommitmentResult)
-        }
-      })
-      .catch(err => {
-        reject(err)
-      })
-    });
-  };
-
   /**
    * Registers an ID given the information from a previous name commitment
    * @param {String} coin The chainTicker of the coin that the ID is based on
